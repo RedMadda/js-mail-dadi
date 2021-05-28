64a920a35840bb50587868dabfc6837d44b75cdf
RedMadda <maddalenarossaro91@gmail.com>
2021-05-29 00:54:13 +0200

    prove per fine settimana, quando nessuno vede

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+// Dò il benvenuto al giocatore con una finestra 
+//modale, poi compare sfondo drago Shenron. 
+// 2) Fa comparire un fumetto, 
+// "Benvenuto! Io sono Shenron. Sfidami nell'antica 
+//  rappresentazione del fato dei dadi, per decidere
+//  le sorti dell'umanità." 
 
+//  bottone accetta con lampadina accesa, spenta no
+// Rifiuta: modale con "Cosa ci fai qui? Here?s a potatoe!!"
+// se accetta, parte gioco.
 // Genero numero random giocatore
 var numUmano = Math.floor(Math.random() * 6) + 1;
 // console.log("Numero giocatore", numUmano);
@@ -20,3 +29,15 @@ if (numUmano > numPc){
 }
 // console.log(esito);
 alert(esito);
+
+// XX) Finestra modale tenera si saluto: "È stato 
+// bello per te come lo è stato per me?"
+
+// XX) Allora rigiochiamo, basta ricaricare la pagina!")  
+// XX) Altra finestra modale con sfondo le sfere
+//      del drago e macchinetta per trovarle.
+//      messaggio:"Meglio che dover ritrovare le 
+//      sfere del drago, no? :)" "Clicca la rotellina
+//      in alto a destra, e risfidami, terrestre!"
+
+
