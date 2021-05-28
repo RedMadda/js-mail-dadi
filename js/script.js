c2b753400bf9e0a974942db1110182775a11cb79
RedMadda <maddalenarossaro91@gmail.com>
2021-05-28 15:22:18 +0200

    punti e virgola

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
 // Genero numero random giocatore
 var numUmano = Math.floor(Math.random() * 7) + 1;
 // console.log("Numero giocatore", numUmano);
+document.getElementById("stampaUmano").innerHTML = "Numero giocatore: " + numUmano;
 
 // Genero numero pc
 var numPc = Math.floor(Math.random() * 7) + 1;
 // console.log("Numero avversario", numPc);
+document.getElementById("stampaPc").innerHTML = "Numero avversario: " + numPc;
+
 
 // stabilire vincitore
 var esito = "Pareggio, nessun vincitore!";
@@ -16,3 +19,4 @@ if (numUmano > numPc){
     esito = "Hai perso!"
 }
 // console.log(esito);
+alert(esito)
