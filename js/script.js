bf232dceeb803a6c78d6841f3403b34b0742b3f7
RedMadda <maddalenarossaro91@gmail.com>
2021-05-28 16:15:18 +0200

    random rivisto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 // Genero numero random giocatore
-var numUmano = Math.floor(Math.random() * 7) + 1;
+var numUmano = Math.floor(Math.random() * 6) + 1;
 // console.log("Numero giocatore", numUmano);
 document.getElementById("stampaUmano").innerHTML = "Numero giocatore: " + numUmano;
 
 // Genero numero pc
-var numPc = Math.floor(Math.random() * 7) + 1;
+var numPc = Math.floor(Math.random() * 6) + 1;
 // console.log("Numero avversario", numPc);
 document.getElementById("stampaPc").innerHTML = "Numero avversario: " + numPc;
 
